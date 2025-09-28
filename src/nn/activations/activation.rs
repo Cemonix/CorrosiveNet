75ca0ac12e3b2dc07d199e4037bc4a584aebb66e
Cemonix <gittlermilan@seznam.cz>
2025-09-28 20:42:52 +0200

    Remove unused duplicite Activation and LossFunction traits from the neural network module

--- src/nn/activations/activation.rs
+++
@@ -1,6 +0,0 @@
-use crate::math::{Matrix, MatrixError};
-
-pub trait Activation<T> {
-    fn forward(&self, input: &Matrix<T>) -> Result<Matrix<T>, MatrixError>;
-    fn backward(&self, input: &Matrix<T>, grad_output: &Matrix<T>) -> Result<Matrix<T>, MatrixError>;
-}
--- src/nn/loss/loss_function.rs
+++
@@ -1,6 +0,0 @@
-use crate::math::{Matrix, MatrixError};
-
-pub trait LossFunction<T> {
-    fn forward(&self, predictions: &Matrix<T>, targets: &Matrix<T>) -> Result<T, MatrixError>;
-    fn backward(&self, predictions: &Matrix<T>, targets: &Matrix<T>) -> Result<Matrix<T>, MatrixError>;
-}
